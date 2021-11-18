f6e078817e0bc2f94cd74fc21a88febdb75d5667
nam <namgd69@gmail.com>
2021-11-18 11:37:24 +0700

    binding project

---
+++ src/main/java/com/example/binding/BindingApplication.java
@@ -0,0 +1,13 @@
+package com.example.binding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BindingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BindingApplication.class, args);
+    }
+
+}
